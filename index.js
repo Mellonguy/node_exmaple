35d01e7c4d6c4a2120cf6efcca391ec19da72449
mellongury <mellonguy@gmail.com>
2019-06-14 16:43:04 +0900

    test init commit

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 'use strict';
 
-var express = require('express');
+let express = require('express');
+let bodyparser = require('body-parser');
 
-var app = express();
+let app = express();
+
+/**
+ * body parser start
+ */
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.post('/write',(req,res)=>{
+    res.send(req.body);
+
+});
+
+/**
+ * body parser end
+ */
 
 app.get('/', function(req, res) {
   res.send("Hello Realm");
 });
+ 
+
+app.get('/write',(req,res)=>{
+    res.sendFile(__dirname+'/write.html')
+});
+ 
 
 app.listen(3000, function() {
   console.log("Go!");
