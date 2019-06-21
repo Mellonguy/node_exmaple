cdf630466cdf6ee715da65f59d4d99db973c85d3
mellongury <mellonguy@gmail.com>
2019-06-21 16:58:40 +0900

    test

--- express_myapp/routes/data.js
+++ express_myapp/routes/data.js
@@ -1,47 +1,38 @@
 var express = require('express'); 
+var router = express.Router();
 
+const{Client} = require('pg');
 
-const{Pool, Client} = require('pg');
-
-const pool = new Pool({
+const client = new Client({
     host: '61.255.238.83',
     user: 'postgres',
-    password: 'P@ssword',
+    password: 'P@ssw0rd',
     database: 'postgres',
     port: 5432
 });
 
-pool.query('select * from iftg.adtn_prod_lst', (err,res)=>{
-    console.log(err,res);
-    pool.end();
-});
-
- 
-var router = express.Router(); 
+const rows = [];
 
 /* GET home page. */ 
-router.get('/', function(_request, response, _next) { 
-    console.log('${id}===============>','${id}');
-    var con = 'postgres://:postgres:P@ssw0rd@61.255.238.83:5432/postgres'; 
-    console.log('con >>>>>>>>>>>>>>>' ,con);
-    pg.connect(con, function(_err, client) { 
+router.get('/', function(request, response, next) { 
+    client.connect();
 
-        console.log('client ==================>',client);
+    var query = client.query('select * from iftg.adtn_prod_lst', (err,res)=>{
+        console.log(err,res);       
+    });
 
-        var query = client.query('select * from iftg.adtn_prod_lst;'); 
-        var rows = []; query.on('row', function(row) { rows.push(row); }); 
-        
-         query.on('end', function(_row,_err) { 
-            response.render('data', { title: 'Express', data:rows }); 
-        }); 
-        query.on('error', function(error) { 
-            console.log("ERROR!!" + error); 
-            response.render('data', { title: title, data: null, message: "ERROR is occured!" }); 
-        }); 
+    query.on('row', function(row) { rows.push(row); });
 
-        if(_err){
-            console.error(_err); 
-        }
+    query.on('end', function(row,err) { 
+        response.render('data', { title: 'Express', data:rows }); 
+        client.end();
     }); 
+ 
+    query.on('error', function(error) { 
+        console.log("ERROR!!" + error); 
+        response.render('data', { title: title, data: null, message: "ERROR is occured!" }); 
+    }); 
+
+ 
 }); 
-module.exports = router;
+module.exports = router; 
