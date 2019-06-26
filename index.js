66859962a79ad59d5676e2679e32ac263426bd23
mellonguy <mellonguy@gmail.com>
2019-06-27 08:42:58 +0900

    .

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-'use strict';
+const fs = require('fs');
 
-let express = require('express');
-let bodyparser = require('body-parser');
-
-let app = express();
-
-/**
- * body parser start
- */
-app.use(bodyparser.urlencoded({extended:true}));
-
-app.post('/write',(req,res)=>{
-    res.send(req.body);
-
-});
-
-/**
- * body parser end
- */
-
-app.get('/', function(req, res) {
-  res.send("Hello Realm");
-});
- 
-
-app.get('/write',(req,res)=>{
-    res.sendFile(__dirname+'/write.html')
-});
- 
-
-app.listen(3000, function() {
-  console.log("Go!");
-});
+fs.access('write.html',(estring)=>console.log(string))
